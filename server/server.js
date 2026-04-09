a2e5d4411672a15d1c52ef862a9581c0005c3465
Your Name <you@example.com>
2026-04-09 17:45:38 +0530

    Add render file

--- server/server.js
+++ server/server.js
@@ -2,13 +2,20 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const path = require('path');
 
 dotenv.config();
 const app = express();
 
 connectDB();
 
-app.use(cors());
+// Handle CORS with wildcard changes to prevent errors
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  credentials: true
+}));
+
 app.use(express.json());
 
 // Import Routes
@@ -16,6 +23,13 @@ app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/menu', require('./routes/menuRoutes'));
 app.use('/api/orders', require('./routes/orderRoutes'));
 app.use('/api/admin/revenue', require('./routes/revenueRoutes'));
+
+// Serve Frontend for Production Deployment
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
